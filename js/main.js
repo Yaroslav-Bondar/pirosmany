ded152ab3de6af225bc8cbe28784b20e966327f8
Yaroslav Bondar <elboxmail@gmail.com>
2021-05-13 07:41:07 +0300

    added header adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+let navigation = $('.navigation')
 let navigationLinkDropdown = document.querySelector('.navigation__link_dropdown')
 let navigationDropdown = document.querySelector('.navigation__dropdown')
+let widthHideNav = 900
 navigationLinkDropdown.addEventListener('click', ()=> {
     navigationDropdown.classList.toggle('navigation__dropdown_active')
 })
@@ -7,4 +9,28 @@ $('.video__btn').click(function(){
     $(this).hide()
     $('.video__iframe').attr('src', 'https://www.youtube.com/embed/aZwev9_Y58A?autoplay=1')
 
+})
+
+// logic for main header navigation
+if(window.innerWidth <= widthHideNav) {
+    navigation.hide()
+    $('.table').show()
+    $('.header__burger').show()
+}
+else {
+    navigation.show()
+    $('.table').hide()
+    $('.header__burger').hide()
+}
+$(window).resize( () => {
+    if(window.innerWidth <= widthHideNav) {
+        navigation.hide()
+        $('.table').show()
+        $('.header__burger').show()
+    }
+    else {
+        navigation.show()
+        $('.table').hide()
+        $('.header__burger').hide()
+    }    
 })
