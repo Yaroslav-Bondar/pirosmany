8f7a63a5f78a6898869771559bc3844eca6734a4
Yaroslav-Bondar <jamnetbox@gmail.com>
2021-07-19 00:58:24 +0300

    pirosmany_0_1_4

--- js/cabinet.js
+++ js/cabinet.js
@@ -1,5 +1,10 @@
 let tabBtns = document.querySelector('.tab__btns')
+let tabContent = document.querySelectorAll('.tab__content')
+
 tabBtns.firstElementChild.classList.add('tab__btn_active')
+
+tabContent.forEach(item => item.style.display = 'none')
+tabContent[0].style.display = 'flex'
 
 tabBtns.addEventListener('click', (e)=> {
     tar = e.target
@@ -10,17 +15,27 @@ tabBtns.addEventListener('click', (e)=> {
         if(child.classList.contains('tab__btn_active')) 
             child.classList.remove('tab__btn_active')
     }
+    tabContent.forEach(item => item.style.display = 'none')
+
     let number = tar.dataset.number
     switch(number) {
         case '0': 
             // tabContent[0].classList.add('tab__content_active')
-            // tabContent[0].style.display = 'block'
+            tabContent[0].style.display = 'flex'
             tar.classList.add('tab__btn_active')
             break
         case '1': 
             // tabContent[1].classList.add('tab__content_active')
-            // tabContent[1].style.display = 'block'
+            tabContent[1].style.display = 'block'
             tar.classList.add('tab__btn_active')
             break
     }
 })
+// adding order status canseled. Just add the class .cab__history_cancel to cab__history
+
+let cabHistoryCansel = document.querySelectorAll('.cab__history_cancel')
+console.log(cabHistoryCansel[0])
+for (const el of cabHistoryCansel) {
+    el.firstElementChild.children[0].children[4].children[1].innerHTML = 'Отменен'
+}
+// // adding order status canseled. Just add the class .cab__history_cancel to cab__history
