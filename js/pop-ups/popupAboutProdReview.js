620a70daf34a95ba1dbf7d4109a793a491f477b3
Yaroslav-Bondar <jamnetbox@gmail.com>
2022-04-27 01:56:26 +0300

    layout for the review popup

--- js/pop-ups/popupAboutProdReview.js
+++ js/pop-ups/popupAboutProdReview.js
@@ -6,7 +6,7 @@ const popupAboutProdReviewHtml = `
                 method="POST"
                 onsubmit="return false;"
             >
-                <button type="button" class="review__close close-btn" data-modal-close="true">
+                <button type="button" class="review__close btn btn_theme_transparent" data-modal-close="true">
                     <img class="img" src="img/icons/plus.svg" alt="close">
                 </button>
                 <div class="review__title">
@@ -14,60 +14,61 @@ const popupAboutProdReviewHtml = `
                 </div>
 
                 <div class="review__column">
-                    <div class="review__row">
+                    <div class="review__row review__row_align_vertical-center">
                         <div class="review__signature">Ваша оценка:</div>
                         <div class="review__rating rating">
-                            <input class="review__rating-item rating__item" 
-                                type="radio" 
-                                name="rating" 
-                                value="star_1"
-                            >
-                            <input class="review__rating-item rating__item"
-                                type="radio" 
-                                name="rating"
-                                value="star_2"
-                            >
-                            <input class="review__rating-item rating__item" 
-                                type="radio" 
-                                name="rating" 
-                                value="star_3"
-                            >
-                            <input class="review__rating-item rating__item" 
-                                type="radio" 
-                                name="rating" 
-                                value="star_4"
-                            >
-                            <input class="review__rating-item rating__item" 
+                            <input class="review__like rating-item" 
                                 type="radio" 
                                 name="rating" 
                                 value="star_5"
                             >
+                            <input class="review__like rating-item"
+                                type="radio" 
+                                name="rating"
+                                value="star_4"
+                            >
+                            <input class="review__like rating-item" 
+                                type="radio" 
+                                name="rating" 
+                                value="star_3"
+                            >
+                            <input class="review__like rating-item" 
+                                type="radio" 
+                                name="rating" 
+                                value="star_2"
+                            >
+                            <input class="review__like rating-item" 
+                                type="radio" 
+                                name="rating" 
+                                value="star_1"
+                            >
                         </div>
                     </div>
-                    <div class="review__row">
-                        <label class="review__signature">Ваше имя</label>
+                    <div class="review__row review__row_align_vertical-center">
+                        <label class="review__signature">Ваше имя:</label>
                         <div class="review__wrap input-wrap">
-                            <input class="review__input form-input" 
+                            <input class="review__name form-input" 
                                 type="text" 
                                 aria-label="your name"
-                                pattern=""
-                                placeholder="Введите Ваше имя" 
+                                placeholder="Введите Ваше имя:" 
                                 name="name" 
                                 required
                             >
                             <div class="review__check input-check"></div>
                         </div>
                     </div>
-                </div>    
-
-                    <div class="review__wrap">
-                        <label class="review__label">
-                            <span class="review__field-name">Ваш отзыв:</span>
-                            <textarea maxlength="500" name="review-text" form="review__form" rows="8" cols="30"></textarea>
-                        </label>
+                    <div class="review__row">
+                        <label class="review__signature">Ваш отзыв:</label>
+                        <textarea 
+                            class="review__text-area form-input" 
+                            maxlength="500" 
+                            name="review">
+                        </textarea>
                     </div>
-                    <button class="review__btn btn" type="submit">Добавить отзыв</button>
-                </div>
+                    <div class="review__row">
+                        <button class="review__btn btn btn_theme_green" type="submit">Добавить отзыв</button>
+                    </div>
+                </div>    
             </form>
         </div>
     </div>`
